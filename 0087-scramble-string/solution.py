84a294e3838e86f45c572b98cca481edd0b115b2
shiva <shivanantham180605@gmail.com>
2026-03-29 09:44:05 +0000

    Sync LeetCode submission Runtime - 7 ms (90.02%), Memory - 19.6 MB (80.77%)

---
+++ 0087-scramble-string/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    memo = {}
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if (s1, s2) in self.memo: return self.memo[(s1, s2)]
+        if s1 == s2: return True
+        if sorted(s1) != sorted(s2): return False # Quick pruning
+        
+        n = len(s1)
+        for i in range(1, n):
+            # Case 1: No swap at this root
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
+                self.memo[(s1, s2)] = True
+                return True
+            # Case 2: Swapped at this root
+            if self.isScramble(s1[:i], s2[n-i:]) and self.isScramble(s1[i:], s2[:n-i]):
+                self.memo[(s1, s2)] = True
+                return True
+                
+        self.memo[(s1, s2)] = False
+        return False
+
